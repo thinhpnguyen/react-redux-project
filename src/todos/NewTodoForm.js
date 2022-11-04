39072d5a0eb8bc6f53ce6cf6874f0515b1774c59
Thinh Nguyen <thinhnguyen@Thinhs-MacBook-Pro.local>
2022-11-03 23:32:08 -0700

    connect redux to app

--- src/todos/NewTodoForm.js
+++ src/todos/NewTodoForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import { createTodo } from "./actions";
 import "./NewTodoForm.css";
-const NewTodoForm = () => {
+const NewTodoForm = ({ todos, onCreatePressed }) => {
   const [inputValue, setInputValue] = useState("");
   return (
     <div className="new-todo-form">
@@ -8,11 +10,30 @@ const NewTodoForm = () => {
         className="new-todo-input"
         type="text"
         placeholde="Type your new todo here"
-        onChange={(e) => setInputValue(e)}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
       />
-      <button className="new-todo-button">Create Todo</button>
+      <button
+        onClick={() => {
+          const isDuplicateText = todos.some(
+            (todo) => todo.text === inputValue
+          );
+          if (isDuplicateText) return;
+          onCreatePressed(inputValue);
+          setInputValue("");
+        }}
+        className="new-todo-button"
+      >
+        Create Todo
+      </button>
     </div>
   );
 };
 
-export default NewTodoForm;
+const mapStateToProps = (state) => ({ todos: state.todos });
+
+const mapDispatchToProps = (dispatch) => ({
+  onCreatePressed: (text) => dispatch(createTodo(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,17 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./TodoList.css";
 import NewTodoForm from "./NewTodoForm.js";
 import TodoListItem from "./TodoListItem.js";
+import { removeTodo } from "./actions.js";
 
-const TodoList = ({ todos = [{ text: "Hello" }] }) => {
+const TodoList = ({ todos = [], onRemovePressed }) => {
+  console.log("here is todo", todos);
   return (
     <div className="list-wrapper">
       <NewTodoForm />
       {todos.map((todo) => (
-        <TodoListItem todo={todo} />
+        <TodoListItem todo={todo} onRemovePressed={onRemovePressed} />
       ))}
     </div>
   );
 };
 
-export default TodoList;
+const mapStateToProps = (state) => ({ todos: state.todos });
+const mapDispatchToProps = (dispatch) => ({
+  onRemovePressed: (text) => dispatch(removeTodo(text)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
